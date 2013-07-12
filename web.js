cbda01d37e30c375551f90c89c21f53ce374126d
Alex Zapata <alex.zapata43@gmail.com>
2013-07-12 03:14:56 +0000

    removed newline from index.html and added code to web.js to read from index.html

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
+var buf = fs.readFileSync('index.html');
+var indexHtml = buf.toString();
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(indexHtml);
 });
 
 var port = process.env.PORT || 5000;
